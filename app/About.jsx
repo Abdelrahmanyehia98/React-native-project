900fe49b4420bc4840917147831033bb6daec589
Mustafa Mahmoud AbdElaziz <mustafadolseka491@gmail.com>
2025-04-09 20:05:24 +0200

    Update About Page Design

--- app/About.jsx
+++ app/About.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../components/Sidebar";
 export default function About() {
   const teamMembers = [
     'Abdo yehia',
-    'Mustafa Dolseka',
+    'Mustafa M. Abd elaziz',
     'Mohamed',
     'Assem Mido',
     'Teto',
@@ -14,9 +14,9 @@ export default function About() {
     <View style={styles.container}>
       <Sidebar />
       <View style={styles.content}>
-        <Text style={styles.text}>About Page</Text>
+        <Text style={styles.title}>About Page</Text>
         <View style={styles.teamMembers}>
-          <Text style={styles.team}>This is our team that contains:</Text>
+          <Text style={styles.subtitle}>This is our team that contains:</Text>
           <View style={styles.list}>
             {teamMembers.map((member, index) => (
               <View key={index} style={styles.listItem}>
@@ -32,34 +32,51 @@ export default function About() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, flexDirection: "row" },
-  content: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#121212",
   },
-  team: {
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1E1E1E",
+    padding: 20,
+    margin: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFD700",
+    marginBottom: 20,
+  },
+  subtitle: {
     fontSize: 18,
     marginBottom: 5,
-  },
-  member: {
-    fontSize: 18,
+    color: "#FFF",
   },
   teamMembers: {
     marginTop: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   list: {
     marginTop: 10,
   },
   listItem: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: "row",
+    alignItems: "flex-start",
     marginBottom: 5,
   },
   bullet: {
     fontSize: 20,
     marginRight: 5,
     marginTop: -3,
+    color: "#FFD700",
+  },
+  member: {
+    fontSize: 18,
+    color: "#FFF",
   },
 });
