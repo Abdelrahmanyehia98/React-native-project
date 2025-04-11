0edfd635e4e2d1d36289a57bcfefc305193901fc
Abdelrahman yehia <abdelrahmanyahya98@gmail.com>
2025-04-11 18:17:13 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ export default function Layout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Sidebar" options={{ title: "sidebar" }} />
       <Stack.Screen name="Home" options={{ title: "Home" }} />
+      <Stack.Screen name="index" options={{ title: "index" }} />
       <Stack.Screen name="About" options={{ title: "About" }} />
       <Stack.Screen name="scroll-view" options={{ title: "Scroll View" }} />
       <Stack.Screen name="login" options={{ title: "Login" }} />
